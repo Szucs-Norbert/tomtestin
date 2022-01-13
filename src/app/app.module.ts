ad0b156227c6fc95513535938bc9409da5a66601
Szucs-Norbert <szucs.norb3rt0424@gmail.com>
2022-01-13 09:24:54 +0100

    Angular-Feladat0323

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,3 +1,14 @@
+/*
+*File:app.module.ts
+*Author: Szűcs Norbert
+*Copyright: 2022, Szűcs Norbert
+*Group: Szoft II/N
+*Date:2022.01.13
+*Github:https://github.com/Szucs-Norbert
+*Feladat: 0323
+*Program feladata: Testömegindex kiszámítása
+*/
+
 import { NgModule } from '@angular/core';
 import { FormControl, ReactiveFormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
